106c76a3a3ec5a01d8117db924c2ab85ac1363e6
olekscode <olk.zaytsev@gmail.com>
2020-02-09 19:55:10 +0100

    Updates

---
+++ precache-manifest.48f4f8cec5fee073db6ff5780f5a231f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca13af8710e048836f7f39aeb478cc6",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "c70904c201070e341177",
+    "url": "/react-tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "15203bda0516f59f2c96",
+    "url": "/react-tictactoe/static/js/2.4898c62b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tictactoe/static/js/2.4898c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70904c201070e341177",
+    "url": "/react-tictactoe/static/js/main.bcdf5584.chunk.js"
+  },
+  {
+    "revision": "b7e318db9431120744bc",
+    "url": "/react-tictactoe/static/js/runtime-main.2fddfb49.js"
+  }
+]);
